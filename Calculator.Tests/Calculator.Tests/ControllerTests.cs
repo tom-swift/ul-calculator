38533487e7d0559336123415b692dfd32879a0e1
Tom Swift <tom@tintinet.co.uk>
2023-10-21 15:05:55 +0100

    Add first failing test
    This is a one off to demonstrate the use of RGR. In future commits I will avoid committing broken code.

---
+++ Calculator.Tests/Calculator.Tests/ControllerTests.cs
@@ -0,0 +1,24 @@
+using AutoFixture;
+using FluentAssertions;
+using Moq.AutoMock;
+
+namespace Calculator.Tests;
+
+public class EvaluateControllerTests
+{
+    private readonly Fixture _fixture = new();
+    private readonly AutoMocker _mocker = new();
+    [Fact]
+    public void Get_Returns_Integer()
+    {
+        // Arrange
+        var subject = _mocker.CreateInstance<EvaluateController>();
+
+        // Act
+        var result = subject.Get();
+
+        // Assert
+        result.Should().BeOfType(typeof(int));
+
+    }
+}
---
+++ Calculator/EvaluateController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator;
+
+[ApiController]
+[Route("[controller]")]
+public class EvaluateController : ControllerBase    
+{
+    public int Get()
+    {
+        throw new NotImplementedException();
+    }
+}
