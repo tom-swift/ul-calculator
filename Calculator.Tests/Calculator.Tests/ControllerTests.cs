ab977d3ebbaa7ae161e1c11fde2f3f47e36556b5
Tom Swift <tom@tintinet.co.uk>
2023-10-22 13:02:31 +0100

    Refactor test file This was purely for readability in the CI output. I may make this more granular as we add more operator types.

--- Calculator.Tests/Calculator.Tests/ControllerTests.cs
+++ Calculator.Tests/Calculator.Tests/ControllerTests.cs
@@ -2,17 +2,57 @@ namespace Calculator.Tests;
 
 public class EvaluateControllerTests
 {
-    private readonly Fixture _fixture = new();
     private readonly AutoMocker _mocker = new();
 
     [Theory]
     [InlineData("", 0)]
+    public void EmptyInputString_Returns_ExpectedOutput(string input, double expectedOutput)
+    {
+        // Arrange
+        var subject = _mocker.CreateInstance<EvaluateController>();
+
+        // Act
+        var result = subject.Get(input);
+
+        // Assert
+        result.Should().Be(expectedOutput);
+    }
+    
+    [Theory]
     [InlineData("1", 1)]
     [InlineData("25", 25)]
+    public void SingleNumberInputString_Returns_ExpectedOutput(string input, double expectedOutput)
+    {
+        // Arrange
+        var subject = _mocker.CreateInstance<EvaluateController>();
+
+        // Act
+        var result = subject.Get(input);
+
+        // Assert
+        result.Should().Be(expectedOutput);
+    }
+    
+    [Theory]
     [InlineData("1+1", 2)]
     [InlineData("1+1+1", 3)]
     [InlineData("2-1", 1)]
-    public void InputString_Returns_ExpectedOutput(string input, double expectedOutput)
+    [InlineData("1-1", 0)]
+    public void SingleOperatorTypeInputString_Returns_ExpectedOutput(string input, double expectedOutput)
+    {
+        // Arrange
+        var subject = _mocker.CreateInstance<EvaluateController>();
+
+        // Act
+        var result = subject.Get(input);
+
+        // Assert
+        result.Should().Be(expectedOutput);
+    }
+    
+    [Theory]
+    [InlineData("2-1+1", 2)]
+    public void MultipleOperatorTypeInputString_Returns_ExpectedOutput(string input, double expectedOutput)
     {
         // Arrange
         var subject = _mocker.CreateInstance<EvaluateController>();
